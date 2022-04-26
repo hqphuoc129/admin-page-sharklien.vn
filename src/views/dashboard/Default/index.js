4de02a5a3c45beb5cf031b777564a4154e68e887
Eric Dang <ericdang2709@gmail.com>
2022-04-27 01:18:25 +0700

    First commit

---
+++ src/views/dashboard/Default/index.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+// material-ui
+import { Grid } from '@mui/material';
+
+// project imports
+import EarningCard from './EarningCard';
+import PopularCard from './PopularCard';
+import TotalOrderLineChartCard from './TotalOrderLineChartCard';
+import TotalIncomeDarkCard from './TotalIncomeDarkCard';
+import TotalIncomeLightCard from './TotalIncomeLightCard';
+import TotalGrowthBarChart from './TotalGrowthBarChart';
+import { gridSpacing } from 'store/constant';
+
+// ==============================|| DEFAULT DASHBOARD ||============================== //
+
+const Dashboard = () => {
+    const [isLoading, setLoading] = useState(true);
+    useEffect(() => {
+        setLoading(false);
+    }, []);
+    return <div>hehheeh</div>;
+};
+
+export default Dashboard;
