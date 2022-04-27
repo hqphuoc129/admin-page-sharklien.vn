d6a32bf33be0bd1da66e6af9dac1ce823fd25d01
hqphuoc129 <hqphuoc129@gmail.com>
2022-04-27 16:27:22 +0700

    add get images collection

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,60 +1,74 @@
-import { lazy } from 'react';
+import { lazy } from "react";
 
 // project imports
-import MainLayout from 'layout/MainLayout';
-import Loadable from 'ui-component/Loadable';
+import MainLayout from "layout/MainLayout";
+import Loadable from "ui-component/Loadable";
 
 // dashboard routing
-const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
-const News = Loadable(lazy(() => import('views/news/index')));
-const Media = Loadable(lazy(() => import('views/media/index')));
+const DashboardDefault = Loadable(
+  lazy(() => import("views/dashboard/Default"))
+);
+const News = Loadable(lazy(() => import("views/news/index")));
+const Images = Loadable(lazy(() => import("views/images/index")));
+p;
+const Videos = Loadable(lazy(() => import("views/videos/index")));
 
 // utilities routing
-const UtilsTypography = Loadable(lazy(() => import('views/utilities/Typography')));
-const UtilsColor = Loadable(lazy(() => import('views/utilities/Color')));
-const UtilsShadow = Loadable(lazy(() => import('views/utilities/Shadow')));
-const UtilsMaterialIcons = Loadable(lazy(() => import('views/utilities/MaterialIcons')));
-const UtilsTablerIcons = Loadable(lazy(() => import('views/utilities/TablerIcons')));
+const UtilsTypography = Loadable(
+  lazy(() => import("views/utilities/Typography"))
+);
+const UtilsColor = Loadable(lazy(() => import("views/utilities/Color")));
+const UtilsShadow = Loadable(lazy(() => import("views/utilities/Shadow")));
+const UtilsMaterialIcons = Loadable(
+  lazy(() => import("views/utilities/MaterialIcons"))
+);
+const UtilsTablerIcons = Loadable(
+  lazy(() => import("views/utilities/TablerIcons"))
+);
 
 // ==============================|| MAIN ROUTING ||============================== //
 
 const MainRoutes = {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-        {
-            path: '/',
-            element: <DashboardDefault />
-        },
-        {
-            path: '/media',
-            element: <Media />
-        },
-        {
-            path: '/news',
-            element: <News />
-        },
-        {
-            path: '/utils/util-typography',
-            element: <UtilsTypography />
-        },
-        {
-            path: '/utils/util-color',
-            element: <UtilsColor />
-        },
-        {
-            path: '/utils/util-shadow',
-            element: <UtilsShadow />
-        },
-        {
-            path: '/icons/tabler-icons',
-            element: <UtilsTablerIcons />
-        },
-        {
-            path: '/icons/material-icons',
-            element: <UtilsMaterialIcons />
-        }
-    ]
+  path: "/",
+  element: <MainLayout />,
+  children: [
+    {
+      path: "/",
+      element: <DashboardDefault />,
+    },
+    {
+      path: "/images",
+      element: <Images />,
+    },
+    {
+      path: "/videos",
+      element: <Videos />,
+    },
+    {
+      path: "/news",
+      element: <News />,
+    },
+    {
+      path: "/utils/util-typography",
+      element: <UtilsTypography />,
+    },
+    {
+      path: "/utils/util-color",
+      element: <UtilsColor />,
+    },
+    {
+      path: "/utils/util-shadow",
+      element: <UtilsShadow />,
+    },
+    {
+      path: "/icons/tabler-icons",
+      element: <UtilsTablerIcons />,
+    },
+    {
+      path: "/icons/material-icons",
+      element: <UtilsMaterialIcons />,
+    },
+  ],
 };
 
 export default MainRoutes;
--- src/views/images/index.js
+++ src/views/images/index.js
@@ -1,16 +1,40 @@
-import {useEffect, useState} from 'react';
-
-// material-ui
-import {Grid} from '@mui/material';
-
-// ==============================|| DEFAULT DASHBOARD ||============================== //
+import { DataGrid } from "@mui/x-data-grid";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Images = () => {
-    const [isLoading, setLoading] = useState(true);
-    useEffect(() => {
-        setLoading(false);
-    }, []);
-    return <div>Images</div>;
+  const [data, setData] = useState([]);
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 200 },
+    { field: "collectionName", headerName: "Collection Name", width: 200 },
+    { field: "mediaList", headerName: "Media List", width: 200 },
+  ];
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://sharklien-backend.herokuapp.com/api/media/get-all-media-collection/image"
+      )
+      .then((response) => {
+        setData(response?.data?.data || []);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log(data);
+
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+  );
 };
 
 export default Images;
